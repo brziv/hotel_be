b8749f73f54683f8c41e0793d41e61ba49cc6be8
redbuttape <quangan0702@gmail.com>
2025-03-07 17:31:33 +0700

    :D

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using hotel_be.ModelFromDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace hotel_be.Controllers
 {
@@ -95,5 +96,44 @@ namespace hotel_be.Controllers
             dbc.SaveChanges();
             return Ok(new { data = booking });
         }
+
+
+        [HttpPost]
+        [Route("Checkin")]
+        public IActionResult Checkin(Guid id)
+        {
+
+            dbc.Database.ExecuteSqlRaw("EXEC pro_check_in {0}", id);
+
+            return NoContent();
+        }
+        [HttpPost]
+        [Route("Checkout")]
+        public IActionResult Checkout(Guid id, String paymethod)
+        {
+
+            dbc.Database.ExecuteSqlRaw("EXEC pro_check_out {0}, {1}", id, paymethod);
+
+            return NoContent();
+        }
+
+        public class ServiceDto
+        {
+            public Guid ServiceID { get; set; }
+            public int Quantity { get; set; }
+        }
+        [HttpPost]
+        [Route("AddService")]
+        public IActionResult AddService(Guid BookingID, [FromBody] List<ServiceDto> services)
+        {
+            foreach (var service in services)
+            {
+                dbc.Database.ExecuteSqlRaw("EXEC pro_edit_services {0}, {1}, {2}",
+                                            BookingID, service.ServiceID, service.Quantity);
+            }
+
+            return NoContent();
+        }
+
     }
 }
